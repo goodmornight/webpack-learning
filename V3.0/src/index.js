acea2129466a8000d472593aa775b8c95bd2675a
GoodNight <goodnightlilanhui@gmail.com>
2021-02-06 22:27:33 +0800

    feat: plugins

---
+++ V3.0/src/index.js
@@ -0,0 +1,4 @@
+import './font/iconfont.css';
+
+let root = document.getElementById('root');
+root.innerHTML = '<span class="iconfont icon-che"></span>'
---
+++ V3.0/webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js'
+  },
+  module: {
+    rules: [{
+      test: /\.(svg|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          name: '[name]_[hash].[ext]',
+          outputPath: 'images/',
+          limit: 1024
+        }
+      }
+    }, {
+      test:/\.css$/,
+      use: [
+        'style-loader', 
+        {
+          loader: 'css-loader',
+          options: {
+          }
+        }]
+    }, {
+      test:/\.(eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'file-loader'
+      }
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
