c6f99413ac8e670a7b523f50f96a9b265dc2ba58
GoodNight <goodnightlilanhui@gmail.com>
2021-02-09 17:17:32 +0800

    feat: babel&react

---
+++ V3.7/webpack.config.js
@@ -0,0 +1,71 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true,
+    port: 3000,
+    hot: true,
+    hotOnly: true
+  },
+  module: {
+    rules: [{
+      test: /\.(svg|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          name: '[name]_[hash].[ext]',
+          outputPath: 'images/',
+          limit: 1024
+        }
+      }
+    }, {
+      test:/\.css$/,
+      use: [
+        'style-loader', 
+        {
+          loader: 'css-loader',
+          options: {
+          }
+        }]
+    }, {
+      test:/\.(eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'file-loader'
+      }
+    }, {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [['@babel/preset-env',
+            {
+              targets: {
+                "chrome": "58",
+                "ie": "11"
+              },
+              useBuiltIns: 'usage'
+            }
+          ]]
+        }
+      }
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
